8180e25ac2ad97dce3158d83fa2fabc45793a5b2
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2025-04-28 11:10:45 -0300

    Create page.js

---
+++ page.js
@@ -0,0 +1,14 @@
+ <div>
+        <h1>Bem-vindo à Página Rota 1</h1>
+        <p>Esta é uma página criada dentro do diretório "app/rota1".</p>
+      </div>
+ 
+    <div>
+      <h1>Bem-vindo à Página Rota 2</h1>
+      <p>Esta é uma página criada dentro do diretório "app/rota2".</p>
+    </div>
+ 
+    <div>
+      <h1>Bem-vindo à Página Rota 3</h1>
+      <p>Esta é uma página criada dentro do diretório "app/rota3".</p>
+    </div>
